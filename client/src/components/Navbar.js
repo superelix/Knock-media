a9a3d2d8c2452d666424bd33eb864a56a8523298
Aniruddha Nath <31971515+superelix@users.noreply.github.com>
2022-02-23 14:58:01 +0530

    Changed some css property

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -59,8 +59,8 @@ const NavBar = ()=>{
      }
     return(
         <nav>
-        <div className="nav-wrapper white">
-          <Link to={state?"/":"/signin"} className="brand-logo left">Instagram</Link>
+        <div className="nav-wrapper">
+          <Link to={state?"/":"/signin"} className="brand-logo left">KNOCK</Link>
           <ul id="nav-mobile" className="right">
              {renderList()}
   
